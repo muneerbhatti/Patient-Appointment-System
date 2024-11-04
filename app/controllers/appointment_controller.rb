ca9f84f6c5fbdaf83fba0a6d27c0513839524e53
muneerbhatti <muneerbhatii350lgu@gmail.com>
2024-11-04 22:34:35 +0500

    doctor index table

--- app/controllers/appointment_controller.rb
+++ app/controllers/appointment_controller.rb
@@ -1,5 +1,4 @@
 class AppointmentController<ApplicationController
-	before_action :authenticate_user!
-	def index
+   def index
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+ devise_for :users
  root 'appointment#index'
+ resources :doctors
+
   
 end
