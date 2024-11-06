4a05db19b947cf19c4441abba7d9da1398f3c404
muneerbhatti <muneerbhatii350lgu@gmail.com>
2024-11-06 17:17:28 +0500

    CREATE PAtient

--- app/controllers/Patients_controller.rb
+++ app/controllers/Patients_controller.rb
@@ -5,4 +5,32 @@ class PatientsController<ApplicationController
     def new
     	@patient=Patient.new
     end
+    def create
+        @patient=Patient.new(Name: params[:patient][:Name],CNIC: params[:patient][:CNIC],
+            PhoneNo: params[:patient][:PhoneNo],date_of_birth: params[:patient][:date_of_birth],
+            Address: params[:patient][:Address],Gender: params[:patient][:Gender] )
+        if @patient.save
+            redirect_to patients_path ,notice: "Patient was successfully created!!.. "
+        else
+            render :new
+        end
+    end
+    def edit
+        @patient=Patient.find(params[:id])
+    end
+    def update
+        @patient=Patient.find(params[:id])
+        if @patient.update(params_patient)
+            redirect_to patients_path,notice:'Patient was successfully Updated!!!...'
+        else
+            render :edit
+        end
+    end
+    def show
+        @patient=Patient.find(params[:id])
+    end
+    private
+    def params_patient
+        params.require(:patient).permit(:Name, :CNIC, :PhoneNo, :Gender, :date_of_birth,:Address)
+    end
 end
