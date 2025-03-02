55ef7b6e9c8f6107840c179c8f5693eb31d5a5b4
muneerbhatti <muneerbhatti350lgu@gmail.com>
2025-03-01 22:44:35 -0500

    modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,6 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-
-
-
   include Pundit::Authorization
 
 
